1e7e101985d044744c650203e8117a77a4f4e897
maheshsabnis <sabnis_m@hotmail.com>
2019-02-10 10:45:01 +0530

    Add files via upload

---
+++ CheckDepartmentTest.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using MVC5_Testing_1.ModelAccess;
+using NUnit.Framework;
+
+namespace MVCModelTest
+{
+    [TestFixture]
+    public class CheckDepartmentTest
+    {
+        [Test]
+        public void CheckDepartmentExist()
+        {
+            // arrange
+            var obj = new DepartmentAccess();
+            // act
+            var Res = obj.CheckDeptExist(10);
+            // assert
+            Assert.That(Res, Is.True);
+        }
+
+        [Test]
+        public void CheckDepartmentExistWithMoq()
+        {
+            //Create Fake Object
+            var fakeObject = new Mock<IDepartmentAccess>();
+            //Set the Mock Configuration
+            //The CheckDeptExist() method is call is set with the Integer parameter type
+            //The Configuration also defines the Return type from the method  
+            fakeObject.Setup(x => x.CheckDeptExist(It.IsAny<string>())).Returns(true);
+            //Call the methid
+            var Res = fakeObject.Object.CheckDeptExist(10);
+
+            Assert.That(Res,Is.True);
+        }
+    }
+}
